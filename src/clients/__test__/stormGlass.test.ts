50d07f335564ea4ac438c28312db20120d4eeff9
Vitor Torino <vitor.torino@compasso.com.br>
2021-05-13 18:51:11 -0300

    Terminada a normalização dos dados
    - Código até aqui correspondendo ao código do [C02P01-2] do Waldemar
    Neto Dev Lab

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -10,7 +10,7 @@ describe("Stormglass client", () => {
         const lat = -33.792726;
         const long = 151.289824;
 
-        axios.get = jest.fn().mockResolvedValue(stormGlassWeather3HoursFixture);
+        axios.get = jest.fn().mockResolvedValue({ data: stormGlassWeather3HoursFixture });
         
         const stormGlass = new StormGlass(axios);
         const response = await stormGlass.fetchPoints(lat, long);
